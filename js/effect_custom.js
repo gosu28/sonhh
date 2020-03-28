4818b4335cb785adf7d08a41a69b6e99bf6649f3
gosu28 <son.bjerg@gmail.com>
2020-03-28 12:22:41 +0700

    update

--- js/effect_custom.js
+++ js/effect_custom.js
@@ -1,4 +1,19 @@
-// window.console = window.console || function(t) {};
-// if (document.location.search.match(/type=embed/gi)) {
-//  	window.parent.postMessage("resize", "*");
-// }
+function refrClock() {
+	var d=new Date();
+	var s=d.getSeconds();
+	var m=d.getMinutes();
+	var h=d.getHours();
+	var day=d.getDay();
+	var date=d.getDate();
+	var month=d.getMonth();
+	var year=d.getFullYear();
+	var days=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+	var months=new Array("1","2","3","4","5","6","7","8","9","10","11","12"); var am_pm;
+	if (s<10) {s="0" + s}
+		if (m<10) {m="0" + m}
+			if (h>12)
+				{h-=12;AM_PM = "PM"}
+			else {AM_PM="AM"}
+				if (h<10) {h="0" + h}
+	document.getElementById("clock").innerHTML=days[day] + ", " + date + "/" +months[month] + "/" + year + ". Now is "+ " [" + h + ":" + m + ":" + s + "] " + AM_PM; 
+setTimeout("refrClock()",1000); } refrClock();
